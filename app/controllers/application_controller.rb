dda794ecf711e6c6f1b507bdb7b5878c6e455dc9
shota.oumaru <otokmae.black@gmail.com>
2020-11-01 09:45:51 +0000

    for heroku

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,4 +8,8 @@ class ApplicationController < ActionController::Base
       redirect_to login_url
     end
   end
+  
+  def counts(user)
+    @count_tasks = user.tasks.count
+  end
 end
--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,26 +1,30 @@
 class TasksController < ApplicationController
   before_action :set_task, only: [:show, :edit, :update, :destroy]
-  
+  before_action :require_user_logged_in
+  before_action :correct_user, only: [:destroy]
   
   def index
-    @tasks = Task.all.page(params[:page]).per(5)
+    if logged_in?
+      @task = current_user.tasks.build  # form_with 用
+      @tasks = current_user.tasks.order(id: :desc).page(params[:page])
+    end
   end
  
   def show
   end
   
-  def new
+  def new 
    @task = Task.new
   end
   
   def create
-    @task = Task.new(task_params)
+    @task = current_user.tasks.build(task_params)
     
     if @task.save
       flash[:success] = "タスクが投稿されました"
       redirect_to @task
     else
-       @task = current_user.tasks.order(id: :desc).page(params[:page])
+      @tasks = current_user.tasks.order(id: :desc).page(params[:page])
       flash[:danger] = "タスクが投稿されません"
       render :new
     end
@@ -41,20 +45,25 @@ class TasksController < ApplicationController
   
   def destroy
     @task.destroy
-    
     flash[:success] = "タスクが削除されました"
-    redirect_to tasks_url
+    redirect_back(fallback_location: root_path)
   end
   
   private
   
   def set_task
-    @task = Task.find(params[:id])
+    @task = Task.find_by(params[:id])
   end
-  
+
+  # Strong Parameter
   def task_params
     params.require(:task).permit(:content, :status)
   end
   
- 
+  def correct_user
+    @task = current_user.tasks.find_by(id: params[:id])
+    unless @task
+      redirect_to root_url
+    end
+  end
 end
